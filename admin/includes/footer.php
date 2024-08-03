d6508110ad78426e2d8b1b05c11e9c050bc8495f
Jeric Javier <152470564+eiirikr@users.noreply.github.com>
2024-08-03 14:44:53 +0800

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2024 <a href="http://isabela.colleges.tripod.com/" target="_blank">Isabela Colleges, Inc.</a></strong>
+</footer>
