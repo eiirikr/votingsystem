969eea7cfd7fe57a2b9c2b7c914447a3bfcf128f
eiirikr <javierjericm@gmail.com>
2024-08-21 11:59:53 +0800

    Add web portfolio

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2024 <a href="http://isabela.colleges.tripod.com/" target="_blank">Isabela Colleges, Inc.</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
